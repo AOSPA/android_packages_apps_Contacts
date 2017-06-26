b5718d4d9370d13bce6e6258f3795fd2dc6c8c06
blong <blong@codeaurora.org>
2017-06-26 14:37:06 +0800

    Fix some contact issues
    - remove platform in mk file
    - correct variable result for more ease understand
    - close cursor in finally
    
    CRs-Fixed: 2066555
    
    Change-Id: I22ab34d140eb635de27445a7ae775ae9d5e46f90

--- Android.mk
+++ Android.mk
@@ -5,7 +5,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src-wrapper)
 LOCAL_JAVA_LIBRARIES := telephony-common
 LOCAL_MODULE := phonebook_wrapper
 LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := platform
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
 include $(CLEAR_VARS)
